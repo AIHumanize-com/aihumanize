a215151329c8d11348d2bf27ff5c6ffd288b9731
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2023-11-28 15:42:08 +0500

    update

--- users/signals.py
+++ users/signals.py
@@ -1,9 +1,24 @@
 from allauth.account.signals import email_confirmed
 from django.dispatch import receiver
+from allauth.socialaccount.models import SocialAccount
+from django.db.models.signals import post_save
 
+from .models import UserModel
 @receiver(email_confirmed)
 def activate_user(sender, request, email_address, **kwargs):
     """Activate user once the email address is confirmed."""
     user = email_address.user
     user.is_active = True
     user.save()
+
+
+
+
+# SocialAccount.objects.filter(user=user).exists()
+
+@receiver(post_save, sender=SocialAccount)
+def activate_user_social(sender, instance, created, **kwargs):
+    if created:
+        user = instance.user
+        user.is_active = True
+        user.save()
