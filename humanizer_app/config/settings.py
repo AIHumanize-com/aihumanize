c3f11686fb508428235f8d79cc06e0ce86da2d98
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2023-12-06 23:36:15 +0500

    redirect

--- humanizer_app/config/settings.py
+++ humanizer_app/config/settings.py
@@ -228,7 +228,7 @@ SOCIALACCOUNT_LOGIN_ON_GET=True
 
 SOCIALACCOUNT_PROVIDERS = {"google": {"SCOPE": ["profile", "email"], "AUTH_PARAMS": {"access_type": "online"}}}
 
-LOGIN_REDIRECT_URL = "/dashboard/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 # ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
 
