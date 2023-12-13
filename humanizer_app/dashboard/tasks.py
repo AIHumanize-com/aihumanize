0f85cfe8eca06a3e94850cfbdb4fa5ee6f092c3d
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2023-12-14 04:12:04 +0500

    bcc email

--- humanizer_app/dashboard/tasks.py
+++ humanizer_app/dashboard/tasks.py
@@ -10,6 +10,8 @@ from datetime import timedelta
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.core.mail import get_connection
+
 @shared_task
 def create_documents_record(input_text, output_text, user_id, purpose, level, readibility):
     """
@@ -49,32 +51,76 @@ def create_documents_record(input_text, output_text, user_id, purpose, level, re
 
 
 
+# @shared_task
+# def send_email_batch(campaign_id):
+#     campaign = EmailCampaign.objects.get(id=campaign_id)
+#     batch_recipient_ids = campaign.recipients[:1000]
+#     remaining_recipient_ids = campaign.recipients[1000:]
+
+#     subject = campaign.subject
+#     from_email = 'updates@aihumanize.com'  # Replace with your actual sender email
+
+#     for user_id in batch_recipient_ids:
+        
+#         user = UserModel.objects.get(id=user_id)
+#         if user.does_email_receive == False:
+#             continue
+#         context = {'subject': subject, 'message': campaign.message, 'user': user}
+#         html_content = render_to_string('dashboard/email_template.html', context)
+        
+#         email = EmailMultiAlternatives(subject, html_content, from_email, [user.email])
+#         email.attach_alternative(html_content, "text/html")
+#         email.send()
+
+#     campaign.recipients = remaining_recipient_ids
+#     campaign.save()
+
+#     if remaining_recipient_ids:
+#         send_email_batch.apply_async((campaign_id,), eta=timezone.now() + timedelta(days=1))
+#     else:
+#         campaign.completed = True
+#         campaign.save()
+
 @shared_task
-def send_email_batch(campaign_id):
+def send_email_batch(campaign_id, start_index=101):
     campaign = EmailCampaign.objects.get(id=campaign_id)
-    batch_recipient_ids = campaign.recipients[:1000]
-    remaining_recipient_ids = campaign.recipients[1000:]
+    all_recipient_ids = campaign.recipients[start_index:]
+
+    # Calculate batch sizes and limits
+    batch_size = 500  # Number of recipients per email
+    daily_limit = 1000  # Half of your daily limit (2000 / 2)
+    daily_recipient_limit = daily_limit * batch_size
+
+    # Determine the range for this batch
+    end_index = min(start_index + daily_recipient_limit, len(all_recipient_ids))
+    batch_recipient_ids = all_recipient_ids[start_index:end_index]
 
     subject = campaign.subject
-    from_email = 'support@aihumanize.com'  # Replace with your actual sender email
+    from_email = 'support@aihumanize.com'
 
-    for user_id in batch_recipient_ids:
-        
-        user = UserModel.objects.get(id=user_id)
-        if user.does_email_receive == False:
-            continue
-        context = {'subject': subject, 'message': campaign.message, 'user': user}
-        html_content = render_to_string('dashboard/email_template.html', context)
-        
-        email = EmailMultiAlternatives(subject, html_content, from_email, [user.email])
-        email.attach_alternative(html_content, "text/html")
-        email.send()
+    # Initialize the SMTP connection
+    with get_connection() as connection:
+        for chunk in range(0, len(batch_recipient_ids), batch_size):
+            current_batch = batch_recipient_ids[chunk:chunk + batch_size]
+            to_emails = []
 
-    campaign.recipients = remaining_recipient_ids
-    campaign.save()
+            for user_id in current_batch:
+                user = UserModel.objects.get(id=user_id)
+                if not user.does_email_receive:
+                    continue
+                to_emails.append(user.email)
 
-    if remaining_recipient_ids:
-        send_email_batch.apply_async((campaign_id,), eta=timezone.now() + timedelta(days=1))
+            if to_emails:
+                
+                context = {'subject': subject, 'message': campaign.message}
+                html_content = render_to_string('dashboard/email_template.html', context)
+                email = EmailMultiAlternatives(subject, html_content, from_email, bcc=to_emails, connection=connection)
+                email.attach_alternative(html_content, "text/html")
+                email.send()
+
+    # Schedule next batch or mark completion
+    if end_index < len(all_recipient_ids):
+        send_email_batch.apply_async((campaign_id, end_index), eta=timezone.now() + timedelta(days=1))
     else:
         campaign.completed = True
         campaign.save()
