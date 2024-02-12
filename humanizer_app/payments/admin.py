3d75477e1a99f5ed25a908400323184dd0c2e3ba
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2024-02-12 20:17:05 +0500

    update

--- humanizer_app/payments/admin.py
+++ humanizer_app/payments/admin.py
@@ -5,10 +5,6 @@ from .models import Subscription, WordCountTracker
 # admin.site.register(WordCountTracker)
 class WordCountTrackerAdmin(admin.ModelAdmin):
     search_fields = ['subscription__user__email']
-    list_filter = ('words_purchased', 'words_used')
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.select_related('subscription__user')
+    
 
 admin.site.register(WordCountTracker, WordCountTrackerAdmin)
