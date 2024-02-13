2f81430293a5ac171535c6e8dcdfcce6f74b28fc
Abdulla Abdurazzoqov <aabdurazzoqov1996@gmail.com>
2024-02-13 21:34:26 +0500

    wordcount bug fix

--- humanizer_app/payments/admin.py
+++ humanizer_app/payments/admin.py
@@ -3,8 +3,11 @@ from .models import Subscription, WordCountTracker
 # Register your models here.
 # admin.site.register(Subscription)
 # admin.site.register(WordCountTracker)
+
+
 class WordCountTrackerAdmin(admin.ModelAdmin):
     search_fields = ['subscription__user__email']
+    autocomplete_fields = ['subscription']
     
 
 admin.site.register(WordCountTracker, WordCountTrackerAdmin)
